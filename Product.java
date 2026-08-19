ce7f85e9e0739e6abe59055297b1b57eaa528b49
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-19 14:55:24 +0200

    ajout du constructeur

--- Product.java
+++ Product.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public abstract class Product {
-        double price;
-        String nameProduct ;
+    double price;
+    String nameProduct ;
+    LocalDate pickingDate;
+    int shelfLifeDays;
+
+    protected Product(double price, String nameProduct, LocalDate pickingDate, int shelfLifeDays) {
+        this.price = price;
+        this.nameProduct = nameProduct;
+        this.pickingDate = pickingDate;
+        this.shelfLifeDays = shelfLifeDays;
+    }
+
+
+
+
+    
+
     
 }
