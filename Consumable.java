4e292637b9875b6813e45de4aeb7248dbc5c2c50
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-20 14:08:47 +0200

    ajout des méthodes de l'interface

--- Consumable.java
+++ Consumable.java
@@ -1,3 +1,10 @@
+import java.time.LocalDate;
+
 public interface Consumable {
-    
+
+    public boolean isRipe();
+
+    public boolean isExpired(LocalDate dateVerification);
+
+    public long daysRemaingBeforeExpiration(LocalDate dateVerification);
 }
