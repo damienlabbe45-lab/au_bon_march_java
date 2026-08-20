73ac2a60de8df9a798e25d5106154591a821ac04
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-20 14:34:15 +0200

    ajout du constructeur

--- VegetableBykg.java
+++ VegetableBykg.java
@@ -1,3 +1,19 @@
-public class VegetableBykg {
+import java.time.LocalDate;
+
+public class VegetableBykg extends Product implements Consumable{
+    double weight;
+
+    public VegetableBykg(double price, String nameProduct, LocalDate pickingDate, int shelfLifeDays, double weight) {
+        super(price, nameProduct, pickingDate, shelfLifeDays);
+        this.weight = weight;
+    }
+
+    
+        
+
+
+
+
+    
     
 }
