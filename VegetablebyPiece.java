7ed2a420588ea3c80ecf138e3527b08d24e3c143
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-20 15:11:17 +0200

    ajout de la classe VegetableBypiece et des méthodes de la classe VegetableBykg

---
+++ VegetablebyPiece.java
@@ -0,0 +1,48 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class VegetableBypiece extends Product implements Consumable{
+    int unit;
+
+    public  VegetableBypiece(double price, String nameProduct, LocalDate pickingDate, int shelfLifeDays, int unit) {
+        super(price, nameProduct, pickingDate, shelfLifeDays);
+        this.unit = unit;
+    }
+    
+    public void weight_minus(int unit){
+        if (this.unit >= unit) this.unit = this.unit - unit;
+        else System.out.println("il me reste juste que " + unit + ".");
+
+    }
+
+    public void calculateExpirationDate(){
+        System.out.println("le produit " + nameProduct +" est à consommer avant le " + pickingDate.plusDays(shelfLifeDays).toString());
+    }
+
+    public boolean isExpired(LocalDate dateVerification){
+        return pickingDate.plusDays(shelfLifeDays).isAfter(dateVerification);
+    }
+    
+    public boolean isRipe(LocalDate dateVerification){
+        return pickingDate.plusDays(4).isEqual(dateVerification);
+    }
+
+    public long daysRemaingBeforeExpiration(LocalDate dateVerification){
+        return ChronoUnit.DAYS.between(pickingDate.plusDays(shelfLifeDays), dateVerification);
+    }
+
+    public double weightpay(int unit){
+        if ( this.unit>= unit){
+            weight_minus(unit);
+            return (price * unit);
+        }
+        return -0.90;
+    
+    }
+
+    @Override
+    public String toString(){
+        return unit + super.toString() + " pièces.";
+    
+    }
+}
